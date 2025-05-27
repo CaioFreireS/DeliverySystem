e43d376baff2c3edef52c84fdccf04421a054f10
Caio <caioFS23@hotmail.com>
2025-05-27 09:20:04 -0300

    fit: implementação desconto por dia da semana

--- src/main/java/org/module/MetodoDescontoPorDiaSemana.java
+++ src/main/java/org/module/MetodoDescontoPorDiaSemana.java
@@ -1,17 +1,31 @@
 package org.module;
 
+import org.model.CupomDescontoEntrega;
 import org.model.Pedido;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class MetodoDescontoPorDiaSemana implements IMetodoDescontoTaxaEntrega {
+    private Map<String, Double> descontoPorDiaSemana;
+
+    public MetodoDescontoPorDiaSemana(){
+        descontoPorDiaSemana=new HashMap<>();
+        descontoPorDiaSemana.put("tuesday", 100.0);
+        descontoPorDiaSemana.put("thursday", 60.0);
+    }
+
     @Override
     public void calcularDesconto(Pedido pedido, Double penalidade) {
+        pedido.addCupomDescontoEntrega(new CupomDescontoEntrega("Desconto por dia da semana "+pedido.getData().getDayOfWeek().toString(), (pedido.getTaxaEntrega()*descontoPorDiaSemana.get(pedido.getData().getDayOfWeek().toString().toLowerCase()))/100-penalidade));
     }
     @Override
     public boolean seAplica(Pedido pedido) {
-        return false;
+        return descontoPorDiaSemana.containsKey(pedido.getData().getDayOfWeek().toString().toLowerCase());
     }
     @Override
     public Double getValorDesconto(Pedido pedido) {
-        return null;
+        return descontoPorDiaSemana.get(pedido.getData().getDayOfWeek().toString().toLowerCase())*pedido.getTaxaEntrega()/100;
     }
 }
