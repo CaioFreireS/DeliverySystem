95197fa8a25594102bca30cbebc6c9c5e420260c
Caio Freire Silva <115713768+CaioFreireS@users.noreply.github.com>
2025-06-05 17:43:32 -0300

    fix: MetodoDescontoValorPedido tratamento de erro

--- src/main/java/org/sistemadescontoentrega/MetodoDescontoValorPedido.java
+++ src/main/java/org/sistemadescontoentrega/MetodoDescontoValorPedido.java
@@ -8,17 +8,26 @@ public class MetodoDescontoValorPedido implements IMetodoDescontoTaxaEntrega {
     private Double VALOR_DESCONTO;
 
     public MetodoDescontoValorPedido(Double limiteValorPedido) {
+        if(limiteValorPedido == null || limiteValorPedido < 0){
+            throw new RuntimeException("ERRO! O limite do valor do pedido não pode ser vazio ou negativo.");
+        }
         this.limiteValorPedido = limiteValorPedido;
         this.VALOR_DESCONTO = 5.0;
     }
 
     @Override
     public void calcularDesconto(Pedido pedido) {
+        if(pedido == null) {
+            throw new RuntimeException("ERRO! Pedido não pode ser nulo");
+        }
         pedido.addCupomDescontoEntrega(new CupomDescontoEntrega("Desconto para Pedido acima de "+limiteValorPedido+" R$", this.VALOR_DESCONTO));
     }
 
     @Override
     public boolean seAplica(Pedido pedido) {
+        if(pedido == null) {
+            throw new RuntimeException("ERRO! Pedido não pode ser nulo");
+        }
         return (pedido.getValorPedido()>limiteValorPedido && pedido.getDescontoEntregaConcedido()<10);
     }
 
