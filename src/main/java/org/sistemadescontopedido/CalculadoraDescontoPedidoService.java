5fc1bf1b9af6940aaba1fda145a1774c1d1bc06e
Caio Freire Silva <115713768+CaioFreireS@users.noreply.github.com>
2025-06-05 17:45:14 -0300

    fix: sistemaDescontoPedido tratamento de erro

--- src/main/java/org/sistemadescontopedido/CalculadoraDescontoPedidoService.java
+++ src/main/java/org/sistemadescontopedido/CalculadoraDescontoPedidoService.java
@@ -15,6 +15,12 @@ public class CalculadoraDescontoPedidoService {
     }
 
     public void calcularValorDesconto(Pedido pedido, String codigo){
+        if(pedido == null) {
+            throw new RuntimeException("ERRO! Pedido não pode ser nulo");
+        }
+        if (codigo == null || codigo.isEmpty()) {
+            throw new RuntimeException("ERRO! o Codigo do Desconto não pode ser nulo");
+        }
         for (ICategoriaCupomDescontoPedido categoriaCupom : categoriasCupomDescontoPedido){
             if(categoriaCupom.seAplica(pedido,codigo)){
                 categoriaCupom.calcularValorDescontado(pedido,codigo);
