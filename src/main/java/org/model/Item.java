2a59f4df938a320667f5aa917173f93221d6d38d
Caio Freire Silva <115713768+CaioFreireS@users.noreply.github.com>
2025-06-05 16:24:39 -0300

    fix: item tratamento de erro

--- src/main/java/org/model/Item.java
+++ src/main/java/org/model/Item.java
@@ -7,9 +7,21 @@ public class Item {
     private String tipo;
 
     public Item(String nome, Integer quantidade, Double valorUnitario, String tipo){
+        if(nome==null || nome.isEmpty()){
+            throw new RuntimeException("ERRO! Nome não pode ser vazio.");
+        }
         this.nome=nome;
+        if(quantidade==null || quantidade<=0){
+            throw new RuntimeException("ERRO! Quantidade não pode ser vazia ou menor ou igual a 0.");
+        }
         this.quantidade=quantidade;
+        if(valorUnitario==null || valorUnitario<=0){
+            throw new RuntimeException("ERRO! Valor unitario do item não pode ser vazio ou menor ou igual a 0.");
+        }
         this.valorUnitario=valorUnitario;
+        if(tipo==null || tipo.isEmpty()){
+            throw new RuntimeException("ERRO! Tipo do item não pode ser vazio.");
+        }
         this.tipo=tipo;
     }
 
