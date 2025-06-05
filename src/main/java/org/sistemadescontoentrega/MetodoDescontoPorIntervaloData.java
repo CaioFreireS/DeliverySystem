92bb3c9f803ab4b91675438fd685745c6d300b56
Caio Freire Silva <115713768+CaioFreireS@users.noreply.github.com>
2025-06-05 16:50:11 -0300

    fix: MetodoDescontoPorIntervaloData tratamento de erro

--- src/main/java/org/sistemadescontoentrega/MetodoDescontoPorIntervaloData.java
+++ src/main/java/org/sistemadescontoentrega/MetodoDescontoPorIntervaloData.java
@@ -16,19 +16,34 @@ public class MetodoDescontoPorIntervaloData implements IMetodoDescontoTaxaEntreg
         this.valorDesconto = 100.0;
     }
 
-    public MetodoDescontoPorIntervaloData(LocalDate dataInicial, LocalDate dataFinal, double valorDesconto) {
+    public MetodoDescontoPorIntervaloData(LocalDate dataInicial, LocalDate dataFinal, Double valorDesconto) {
+        if(dataInicial == null){
+            throw new RuntimeException("ERRO! Data inicial do Desconto não pode ser nulo");
+        }
         this.dataInicial = dataInicial;
+        if(dataFinal == null){
+            throw new RuntimeException("ERRO! Data final do desconto não pode ser nulo");
+        }
         this.dataFinal = dataFinal;
+        if(valorDesconto == null || valorDesconto < 0){
+            throw new RuntimeException("ERRO! Valor do Desconto não pode ser nulo ou menor ou igual a 0");
+        }
         this.valorDesconto = valorDesconto;
     }
 
     @Override
     public void calcularDesconto(Pedido pedido) {
+        if(pedido == null) {
+            throw new RuntimeException("ERRO! Pedido não pode ser nulo");
+        }
         pedido.addCupomDescontoEntrega(new CupomDescontoEntrega("Desconto do dia: "+dataInicial+"<>"+dataFinal, (pedido.getTaxaEntrega()*valorDesconto)/100));
     }
 
     @Override
     public boolean seAplica(Pedido pedido) {
+        if(pedido == null) {
+            throw new RuntimeException("ERRO! Pedido não pode ser nulo");
+        }
         return pedido.getData().isAfter(dataInicial) && pedido.getData().isBefore(dataFinal);
     }
 
